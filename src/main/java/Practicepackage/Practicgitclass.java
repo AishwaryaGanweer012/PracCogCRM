8e639ef219c135a0407fa0effda011273df2ec20
HARSHINI PENDOTA <Harshinipendota8@gmail.com>
2024-07-21 12:13:16 +0530

    Harshini created practicepackage

---
+++ src/main/java/Practicepackage/Practicgitclass.java
@@ -0,0 +1,9 @@
+package Practicepackage;
+
+public class Practicgitclass {
+
+	public static void main(String[] args) {
+System.out.println("this is a practice package for practiceing git workflow");
+	}
+
+}
